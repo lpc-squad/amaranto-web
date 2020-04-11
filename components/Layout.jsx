f19f24171e4cb467d80aa4d73b7e2fd9cb762cda
FMGordillo <facundomgordillo@gmail.com>
2020-04-10 23:57:09 -0300

    :art: Landing page init

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import {
   AppBar,
   Avatar,
@@ -13,7 +14,7 @@ import {
   ListItemText,
   ListItemIcon,
   Toolbar,
-  Typography
+  Typography,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
@@ -25,7 +26,7 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   avatarOpen: {
     // width: drawerWidth / 2,
     // height: drawerWidth / 2,
@@ -58,47 +59,47 @@ const useStyles = makeStyles(theme => ({
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
-    marginRight: 36
+    marginRight: 36,
   },
   hide: {
-    display: "none"
+    display: "none",
   },
   drawer: {
     display: "flex",
     width: drawerWidth,
     flexShrink: 0,
     whiteSpace: "nowrap",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   drawerOpen: {
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   drawerClose: {
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: "hidden",
     width: 0, // Mobile hidden
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
+      width: theme.spacing(9) + 1,
+    },
   },
   toolbar: {
     display: "flex",
@@ -106,109 +107,121 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
     paddingTop: theme.spacing(3),
     [theme.breakpoints.up("sm")]: {
-      padding: theme.spacing(3)
-    }
-  }
+      padding: theme.spacing(3),
+    },
+  },
 }));
 
 function Layout(props) {
   const theme = useTheme();
+  const router = useRouter();
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [isRoot, setIsRoot] = useState(false);
 
   function toggleDrawer() {
-    setOpen(o => !o);
+    setOpen((o) => !o);
   }
 
-  return (
-    <div style={{ display: "flex" }}>
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={toggleDrawer}
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Clinica Digital
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        anchor="left"
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open
-          })
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={toggleDrawer}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List className={classes.list}>
-          <Link href="/">
-            <ListItem button component="a">
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText>Inicio</ListItemText>
-            </ListItem>
-          </Link>
-          <Link href="/dashboard">
-            <ListItem button component="a">
-              <ListItemIcon>
-                <PatientsIcon />
-              </ListItemIcon>
-              <ListItemText>Pacientes</ListItemText>
-            </ListItem>
-          </Link>
-        </List>
-        <Avatar
-          className={clsx({
-            [classes.avatarOpen]: open,
-            [classes.avatarClose]: !open
+  useEffect(() => {
+    if (router.pathname === "/") {
+      setIsRoot(true);
+    }
+  }, [router, isRoot]);
+
+  if (isRoot) {
+    return props.children;
+  } else {
+    return (
+      <div style={{ display: "flex" }}>
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
           })}
         >
-          JC
-        </Avatar>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        {/* TODO: Fix this for mobile */}
-        <Container>{props.children}</Container>
-      </main>
-    </div>
-  );
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={toggleDrawer}
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Clinica Digital
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor="left"
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={toggleDrawer}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List className={classes.list}>
+            <Link href="/">
+              <ListItem button component="a">
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText>Inicio</ListItemText>
+              </ListItem>
+            </Link>
+            <Link href="/dashboard">
+              <ListItem button component="a">
+                <ListItemIcon>
+                  <PatientsIcon />
+                </ListItemIcon>
+                <ListItemText>Pacientes</ListItemText>
+              </ListItem>
+            </Link>
+          </List>
+          <Avatar
+            className={clsx({
+              [classes.avatarOpen]: open,
+              [classes.avatarClose]: !open,
+            })}
+          >
+            JC
+          </Avatar>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {/* TODO: Fix this for mobile */}
+          <Container>{props.children}</Container>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default Layout;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,57 @@
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
 function Index(props) {
-  return <p>Index</p>;
+  // TODO: Para hacer el efecto Slack https://slack.com/intl/en-ar/
+  // Fuente: https://material-ui.com/components/app-bar/#scrolling
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+  return (
+    <>
+      <header style={{ position: "fixed", zIndex: 9 }}>
+        <a href="">Inicio</a>
+      </header>
+      <main>
+        <section style={{ height: "100vh" }}>
+          <video
+            autoPlay
+            muted
+            loop
+            src="/index_video.mp4"
+            style={{
+              objectFit: "cover",
+              right: "0",
+              bottom: "0",
+              minWidth: "100%",
+              maxHeight: "100%",
+            }}
+          ></video>
+          <div
+            style={{
+              position: "absolute",
+              width: "40%",
+              height: "50%",
+              top: "calc(100% / 4)",
+              backgroundColor: "white",
+              clipPath: "polygon(0% 0%, 50% 0%, 100% 50%, 50% 100%, 0% 100%)",
+            }}
+          >
+            <h1>Clinical Record</h1>
+          </div>
+        </section>
+        <section style={{ display: "flex", height: "75vh" }}>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+        </section>
+        <section style={{ display: "flex", height: "75vh" }}>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+        </section>
+        <section style={{ display: "flex", height: "100vh" }}>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+          <h1 style={{ flex: 1 }}>Otro titulo</h1>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Index;
